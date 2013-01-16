c2665dd055a6e581ec2f2a9dbb3dd175d11f709a
Mat Schaffer <mat@schaffer.me>
2013-01-15 23:52:25 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kickballapp::Application.config.secret_token = '4b096356d531f282490ceda179d5ee7b1e23203dbf95f3fba946d01d37192dfc68608929bbeb803bdec2b9b4d7abbfb0edc6c9ae16d78d0c300d39d63ff6ac4f'
