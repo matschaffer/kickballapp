182f519406e9f16f236cba5d159e13ae6a1f2648
Mat Schaffer <mat@schaffer.me>
2013-02-05 18:04:04 -0500

    Stuff that we should do in class 5

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,8 @@
 class Player < ActiveRecord::Base
   belongs_to :team
-  attr_accessible :first_name, :last_name, :number
+  attr_accessible :first_name, :last_name, :number, :team_id
+
+  def team_name
+    team.name if team
+  end
 end
