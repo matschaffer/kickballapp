8f6bb9130e0622a98cb541578936d4ac9c48b571
Mat Schaffer <mat@schaffer.me>
2013-04-23 23:55:30 -0700

    Avoid asset initialize problem

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,8 @@ module Kickballapp
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     config.generators do |g|
       g.controller_specs false
       g.view_specs false
--- spec/features/ticket_purchasing_spec.rb
+++ spec/features/ticket_purchasing_spec.rb
@@ -3,10 +3,14 @@ require 'spec_helper'
 describe 'Purchasing a ticket' do
   fixtures :teams, :games
 
-  it 'should require sign in' do
+  it 'should allow sign up' do
     visit games_path
     click_on 'Buy a ticket'
     click_on 'Sign up'
+  end
 
+  it 'should accept a credit card', js: true do
+    visit games_path
+    click_on 'Buy a ticket'
   end
 end
